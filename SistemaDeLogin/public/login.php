7922e16cd370c1335d6c072b45eb75ba020c8aff
Marcos Conde <155701351+MarcosCondeJr@users.noreply.github.com>
2024-09-09 01:07:41 -0300

    Update login.php

--- SistemaDeLogin/public/login.php
+++ SistemaDeLogin/public/login.php
@@ -39,20 +39,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 <body>
     <form action="index.php?rota=login" method="post">
         <h3>Login</h3>
-        <div>
+        <div class="usuario">
             <label for="usuario">Usuário</label>
-            <input type="text" name="usuario" id="usuario">
+            <input type="email" name="usuario" id="usuario">
         </div>
-        <div>
+        <div class="password">
             <label for="senha">Senha</label>
             <input type="password" name="senha" id="senha">
         </div>
-        <div>
+        <div class="button">
             <button type="submit">Entrar</button>
         </div>
     </form>
